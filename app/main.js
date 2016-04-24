11f3d13d7ebe10730cb4dd4bf36281059d72e60d
Vitaly Tsvetkov <cvetvitaly@yandex.ru>
2016-04-24 23:00:43 +0300

    add drag'n'drop, some styles

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
